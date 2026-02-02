fcbf5821ad98e7f885d4be79b472de35aa35425d
Jesus <bfvcontacto@gmail.com>
2026-02-02 07:36:45 -0300

    base de datos

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,5 +1,5 @@
 // assets/js/supabase.js
-const SUPABASE_URL = "https://ptleunsqwhsndauiaxex.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_RaQK8PBVnfiwXq-Du3bDRg_KLtdhMVc";
+const SUPABASE_URL = "https://khlxidmskfsgdzmggctw.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_tfFY4GbDOIo5G3hefAYSMQ_Qyi6azRP";
 
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
